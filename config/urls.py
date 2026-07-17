742344de3f669b939e4bf9906b07d4cca7240bc6
[atharvabarbare-ctrl] <[atharvabarbare@gmail.com]>
2026-07-17 17:50:10 +0530

    Hide admin URL using environment variable

--- config/urls.py
+++ config/urls.py
@@ -14,17 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
+
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
 
-    path('admin/', admin.site.urls),
-    path('',include('home.urls')),
+    path(os.environ.get("ADMIN_URL", "admin/"), admin.site.urls),
+    path("", include("home.urls")),
 
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
